0c5a4144cf6dbc3e7b508671e9abbd10b7c428f4
BlipOYC <oweche0809@highgateschool.org.uk>
2025-10-03 10:30:38 +0100

    Started OOP file and Dog Module

--- class_exercises/OOP/Dog OOP.py
+++ class_exercises/OOP/Dog OOP.py
@@ -1,9 +1,25 @@
 #Dog Class Exercises
 
-class Dog:
+
+class Animal:
     def __init__(self, name, age):
         self.name = name
         self.age = age
 
+    def __repr__(self):
+        return f'Name: {self.name}, age: {self.age}'
+
+
+class Dog(Animal):
+#INHERITANCE
     def bark(self):
         return "Woof!"
+
+    def play(self, other):
+        if isinstance(other, Dog):
+            return f"{self.name} is playing with {other.name}!"
+        elif isinstance(other, Animal):
+            return f"{self.name} doesn't know how to play with {other.name}!"
+        else:
+            return f"{self.name} is not interested in playing with {other}!"
+
