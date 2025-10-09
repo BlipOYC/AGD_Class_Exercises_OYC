ac2795fc224b03d846103e66b7e7d80e89cf4b4f
oweche0809 <oweche0809@highgateschool.org.uk>
2025-10-09 10:32:55 +0100

    Fighting Fantasty So far

--- class_exercises/OOP/ff.py
+++ class_exercises/OOP/ff.py
@@ -1,4 +1,26 @@
 import random
+
 def dice_sum(num_dice: int = 1, sides: int = 6):
+    """returns the sum of num_dice dice, each with side sides"""
     return sum(random.randint(1, sides) for _ in range(num_dice))
-print(dice_sum(12))
+
+
+class Character:
+
+    def __init__(self, name : str, skill: int, stamina : int):
+        self.name = name
+        self.skill = skill
+        self.stamina = stamina
+        self.roll = None
+        self.score = None
+
+    def __repr__(self):
+        return f"Character('{self.name}', skill={self.skill}, stamina={self.stamina})"
+
+    def find_score(self):
+        self.roll = dice_sum(2)
+        self.score = self.roll + self.skill
+        return self.score
+
+dragon = Character("Dragon", 10, 22)
+print(dragon)
