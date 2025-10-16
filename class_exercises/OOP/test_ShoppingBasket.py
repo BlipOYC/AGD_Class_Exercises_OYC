6f48094da2e91f879df7d3eea2fbdfe522c62711
BlipOYC <oweche0809@highgateschool.org.uk>
2025-10-16 20:14:52 +0100

    Update test_ShoppingBasket.py

--- class_exercises/OOP/test_ShoppingBasket.py
+++ class_exercises/OOP/test_ShoppingBasket.py
@@ -1,4 +1,6 @@
 import pytest
+from Items import Item
+from ShoppingBasket import ShoppingBasket
 
 @pytest.fixture
 def setup_items_and_basket():
@@ -18,17 +20,84 @@ def setup_items_and_basket():
 
     return basket, tomatoSoup, spaghetti, blackOlives, mozarella, gratedCheese
 
-def test_add_item():
-    assert False
+def test_shopping_basket_setup(setup_items_and_basket):
+    """Checking that the basket is initialized"""
+    basket, tomatoSoup, spaghetti, blackOlives, mozarella, gratedCheese = setup_items_and_basket
+    assert basket.items == {tomatoSoup: 10, blackOlives: 1, mozarella: 2}
 
 
-def test_remove_item():
-    assert False
+def test_add_normal(setup_items_and_basket):
+    """Test basket.addItem() works"""
+    basket, *_, blackOlives, mozarella, gratedCheese = setup_items_and_basket
+    basket.addItem(gratedCheese, 20)
+    basket.addItem(blackOlives, 2)
+    basket.addItem(mozarella, 12)
+    assert basket.items[gratedCheese] == 20
+    assert basket.items[blackOlives] == 3
+    assert basket.items[mozarella] == 14
 
+    #Adding the maximum possible
 
-def test_update_item():
-    assert False
+    basket.addItem(gratedCheese, 9)
+    assert basket.items[gratedCheese] == 29
 
+def test_more_than_stock(setup_items_and_basket):
+    basket, tomatoSoup, *_ = setup_items_and_basket
+    basket.addItem(tomatoSoup, 142857) #Nice number
+    assert basket.items[tomatoSoup] == 20
 
-def test_reset():
-    assert False
+def test_non_positive_add(setup_items_and_basket):
+    basket, tomatoSoup, *_ = setup_items_and_basket
+    with pytest.raises(ValueError):
+        basket.addItem(tomatoSoup, 0)
+
+    with pytest.raises(ValueError):
+        basket.addItem(tomatoSoup, -123)
+
+def test_remove_normal(setup_items_and_basket):
+    basket, tomatoSoup, *_ = setup_items_and_basket
+    basket.removeItem(tomatoSoup, 3)
+    assert basket.items[tomatoSoup] == 7
+    basket.removeItem(tomatoSoup) #In here as it is expected/normal behaviour
+    with pytest.raises(KeyError):
+        basket.items[tomatoSoup] #Perfect, tomatoSoup has been deleted from the dict, hence check by KeyError
+
+def test_remove_edge(setup_items_and_basket):
+    basket, tomatoSoup, spaghetti, blackOlives, *_ = setup_items_and_basket
+    basket.removeItem(tomatoSoup, 11) #More than available
+    basket.removeItem(blackOlives, -2)
+    with pytest.raises(KeyError): #Similar to above
+        basket.items[tomatoSoup]
+    with pytest.raises(KeyError):
+        basket.items[blackOlives]
+    with pytest.raises(ValueError): #Spaghetti was not added
+        basket.removeItem(spaghetti, 4)
+
+def test_update(setup_items_and_basket):
+    basket, tomatoSoup, *_ = setup_items_and_basket
+    basket.updateItem(tomatoSoup, 2)
+    assert basket.items[tomatoSoup] == 2
+    assert tomatoSoup.stock_level == 18
+
+    basket.updateItem(tomatoSoup, 20)
+    assert basket.items[tomatoSoup] == 20
+    assert tomatoSoup.stock_level == 0
+
+def test_update_edge(setup_items_and_basket):
+    basket, tomatoSoup, spaghetti, blackOlives, mozarella, gratedCheese = setup_items_and_basket
+    valids = [tomatoSoup, blackOlives, mozarella]
+    invalids = [spaghetti, gratedCheese]
+    for invalid in invalids:
+        with pytest.raises(ValueError):
+            basket.updateItem(invalid, invalid.stock_level - 1) #So we know the issue isn't because of stock level
+    for valid in valids:
+        basket.updateItem(valid, 0)
+        with pytest.raises(KeyError): #Again, testing for absence
+            basket.items[valid]
+
+def test_reset(setup_items_and_basket):
+    basket, *items = setup_items_and_basket
+    for item in items:
+        basket.addItem(item, item.stock_level) #Max it out (by this point, we can be reasonably sure addItem works)
+    basket.reset()
+    assert basket.items == {}
