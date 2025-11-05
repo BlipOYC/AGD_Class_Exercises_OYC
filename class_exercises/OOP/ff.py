bdc4f9312102a4f723b3354c447fe4147a535c35
BlipOYC <oweche0809@highgateschool.org.uk>
2025-11-05 08:29:28 +0000

    Update ff.py
    Added things to allow pass tests

--- class_exercises/OOP/ff.py
+++ class_exercises/OOP/ff.py
@@ -4,11 +4,10 @@ def dice_sum(num_dice: int = 1, sides: int = 6):
     """returns the sum of num_dice dice, each with side sides"""
     return sum(random.randint(1, sides) for _ in range(num_dice))
 
-
 class Character:
 
     def __init__(self, name : str, skill: int, stamina : int):
-        self.name = name
+        self.name = name.title()
         self.skill = skill
         self.stamina = stamina
         self.roll = None
@@ -16,6 +15,9 @@ class Character:
 
     def __repr__(self):
         return f"Character('{self.name}', skill={self.skill}, stamina={self.stamina})"
+
+    def __str__(self):
+        return self.name
 
     @property
     def is_dead(self):
@@ -29,10 +31,10 @@ class Character:
             self.stamina = max(self.stamina, 1)
 
     def return_character_status(self):
-        return f"{self.name} has a skill {self.skill} and a stamina {self.stamina}"
+        return f"{self.name} has skill {self.skill} and stamina {self.stamina}"
 
     def return_roll_status(self):
-        return f"{self.name} has rolled a {self.roll} for {self.score} total points."
+        return f"{self.name} rolled {self.roll} for a total score of {self.score}"
 
     def find_score(self):
         self.roll = dice_sum(2)
@@ -50,19 +52,32 @@ class Character:
             case [True, False]:
                 print(f"{self.name} has scored a hit! {self.name} takes 2 damage!")
                 other.take_hit()
+                return "Win"
             case [False, True]:
                 print(f"It is a draw! {self.name} and {other.name} both take 1 damage")
                 self.take_hit(1)
                 other.take_hit(1)
+                return "Draw"
             case [False, False]:
                 print(f"{other.name} has scored a hit! {self.name} takes 2 damage!")
                 self.take_hit()
+                return "lost"
+        return None
+
 
 class PlayerCharacter(Character):
 
-    def __init__(self, name, skill, stamina, rnjesus):
+    def __init__(self, name, skill, stamina, luck):
         super().__init__(name, skill, stamina)
-        self.luck = rnjesus
+        self.luck = luck
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', skill={self.skill}, stamina={self.stamina}, luck={self.luck})"
+
+    def test_luck(self):
+        self.luck -= 1
+        self.roll = dice_sum(2)
+        return self.roll <= self.luck+1
 
     @classmethod
     def generate_player_character(cls, name):
@@ -74,8 +89,42 @@ class PlayerCharacter(Character):
 def NonPlayerCharacter(Character):
     pass
 
+class Game:
+    @classmethod
+    def load_creatures(cls):
+        creatures = [Character("Dragon", 10, 22),
+                     Character("Orc", 7, 10),
+                     Character("Skeleton", 5, 8),
+                     Character("Giant Rat", 6, 6),
+                     ]
+        return creatures
+
+    def __init__(self):
+        self.opponent = None
+        self.player = None
+        self.round_result = None
+        self.creatures = self.load_creatures()
+
+    def choose_opponent(self):
+        self.opponent = random.choice(self.creatures)
+        self.creatures.remove(self.opponent)
+
+    def set_player(self, player_character):
+        self.player = player_character
+
+    def resolve_fight_round(self):
+        self.round_result = self.player.fight_round(self.opponent)
+
+    def return_character_status(self, character):
+        name, skill, stamina = character.name, character.skill, character.stamina
+        return f"{name} has skill {skill} and stamina {stamina}"
+
 hero = PlayerCharacter.generate_player_character("Hero")
 dragon = Character("Dragon", 10, 10)
+
+game = Game()
+game.choose_opponent()
+
 """
 while hero.stamina > 0 and dragon.stamina > 0:
     hero.fight_round(dragon)
