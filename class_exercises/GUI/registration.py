1b602f791f56e31d69f8568a578616d296488b5a
oweche0809 <oweche0809@highgateschool.org.uk>
2025-11-17 11:11:09 +0000

    tkintering

--- class_exercises/GUI/registration.py
+++ class_exercises/GUI/registration.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 class MainFrame(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
-        self.title = tk.Label(self, text="Registration Form")
+        self.title = tk.Label(self, text="Registration Form", font=("Arial", 16))
         self.full_name = tk.Label(self, text="Full Name")
         self.enter_name = tk.Entry(self)
         self.email = tk.Label(self, text="Email")
@@ -14,8 +14,41 @@ class MainFrame(tk.Frame):
         self.place_widget()
 
     def place_widget(self):
-        self.title.pack(side=tk.LEFT)
-        self.full_name.pack(side=tk.LEFT)
+        settings = {"stick": "W", "padx": 5, "pady": 5}
+        self.rowconfigure(0, weight=11)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=5)
+
+
+        self.title.grid(row=0, column=0, rowspan=2, stick=tk.EW)
+        self.full_name.grid(row=1, column=0,  **settings)
+        self.enter_name.grid(row=1, column=1, **settings)
+        self.email.grid(row=2, column=0, **settings)
+        self.enter_email.grid(row=2, column=1, **settings)
+
+class GenderFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.gender = tk.Label(self, text="Gender")
+        self.selected_gender = tk.StringVar()
+        self.genders = ("Male", "Female")
+
+        self.place_widget()
+
+    def place_widget(self):
+        settings = {"padx": 5, "pady": 5}
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=3)
+
+        self.gender.grid(row=0, column=0
+                         )
+        for idx, gender in enumerate(self.genders):
+            r = tk.Radiobutton(self, text=gender, variable=self.selected_gender, value=gender)
+            r.grid(row=0, column=idx + 1, stick=tk.EW)
+
 
 
 if __name__ == '__main__':
@@ -24,4 +57,6 @@ if __name__ == '__main__':
     root.geometry("300x200")
     main_frame = MainFrame(root)
     main_frame.pack(fill=tk.BOTH, expand=True)
+    gender_frame = GenderFrame(root)
+    gender_frame.pack(fill=tk.BOTH, expand=True)
     root.mainloop()
